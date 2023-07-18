f59a2dd774120b9cf3d52c314c6a340c557e2798
meemory <mehmetcaran@hotmail.com>
2023-07-18 19:39:28 +0200

    added hamburger menu

--- src/assets/components/navigation/Navbar.jsx
+++ src/assets/components/navigation/Navbar.jsx
@@ -2,14 +2,25 @@ import "./nav.css";
 
 export default function Navbar() {
   return (
-    <div id="menu">
-      <div id="menu-items">
-        <a className="menu-item">/home</a>
-        <a className="menu-item">/tools</a>
-        <a className="menu-item">/projects</a>
-        <a className="menu-item">/about-me</a>
+    <div className="navbar">
+      <div id="hamburger">
+        <input class="checkbox" type="checkbox" name="" id="" />
+        <div class="hamburger-lines">
+          <span class="line line1"></span>
+          <span class="line line2"></span>
+          <span class="line line3"></span>
+        </div>
       </div>
-      <div id="menu-background"></div>
+
+      <div id="menu">
+        <div id="menu-items">
+          <a className="menu-item">/home</a>
+          <a className="menu-item">/tools</a>
+          <a className="menu-item">/projects</a>
+          <a className="menu-item">/about-me</a>
+        </div>
+        <div id="menu-background"></div>
+      </div>
     </div>
   );
 }
