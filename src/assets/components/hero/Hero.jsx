f33436cbdcbd7a2dc6ec6d796581380192f966a3
meemory <mehmetcaran@hotmail.com>
2023-07-20 13:35:22 +0200

    added pictures

--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -4,11 +4,11 @@ export default function Hero() {
   return (
     <div id="hero-container">
       <h1>Mehmet Caran</h1>
-      <h2>Fullstack Developer</h2>
       <p>
-        I build accessiblem inclusive products and digital experiences for the
-        web
+        I build accessible, inclusive products and digital experiences for the
+        web.
       </p>
+      <h2>Fullstack Developer</h2>
     </div>
   );
 }
